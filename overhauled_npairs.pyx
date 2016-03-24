232ddfaeead901e3092c8c0c5aded96a84724a09
Andrew Hearin <andrew.hearin@yale.edu>
2016-03-24 11:56:09 -0700

    Starting to develop overhauled npairs strategy

--- overhauled_npairs.pyx
+++ overhauled_npairs.pyx
@@ -1,2 +1,14 @@
 import numpy as np
 cimport numpy as cnp
+import tree_module 
+
+def cython_npairs(double[:,:] data1, double[:, :] data2, 
+	double[:] rbins, double[:] period):
+	
+	cdef double xperiod = period[0]
+	cdef double yperiod = period[1]
+	cdef double zperiod = period[2]
+	cdef int num_rbins = len(rbins)
+	cdef double rmax = rbins[num_rbins[num_bins-1]]
+
+
